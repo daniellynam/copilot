8f5261710c205656beae1cce1dac3a7bc8690462
daniellynam <daniel.lynam@au.ey.com>
2025-06-17 20:43:29 +1000

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-echo "Hello, World!";
-?>
+// <?php
+// echo "Hello, World!";
+// ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Senate Workbench</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .chat-window {
+            width: 80%;
+            height: 80%;
+        }
+    </style>
+</head>
+<body>
+    <div class="chat-window">
+        <iframe src="https://copilotstudio.microsoft.com/environments/748bab4f-a737-e24a-a461-e28516a5054a/bots/cr4b6_parliamentarySenateEstimatesAssistant/webchat?__version__=2" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+    </div>
+</body>
+</html>
